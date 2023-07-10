0a16290beae0be3ebfe7cec75364495fd1fd584e
Lucas Hope <115725533+lucasfhope@users.noreply.github.com>
2023-07-09 23:36:48 -0400

    commented

--- day6/day6_pt1.kt
+++ day6/day6_pt1.kt
@@ -15,7 +15,11 @@ fun main() {
     println("After following Santa's instructions, there were $lightCount lights left on.")   /* 569999 */
 }
 
-fun executeLightGridCommand(lightGrid: Array<Array<Boolean>> , input: String) {
+
+/* executeLightGridCommand() parses the cmd read 
+   from input.txt so the lightGrid can be updated  */
+
+fun executeLightGridCommand(lightGrid: Array<Array<Boolean>>, input: String) {
     
     val input_split = input.split(" ")
     val cmd_len = input_split.size
@@ -42,9 +46,16 @@ fun executeLightGridCommand(lightGrid: Array<Array<Boolean>> , input: String) {
     turnOnOffLightGrid(lightGrid, start_indices[0].toInt(), start_indices[1].toInt(), end_indices[0].toInt(), end_indices[1].toInt(), turnOn)
 }
 
+
+/* createLightGrid() returns a turned 
+   off lightGrid of dimensions specified  */ 
+
 fun createLightGrid(x: Int, y: Int): Array<Array<Boolean>> {
     return Array(x) { Array(y) {false} }
 }
+
+/* turnOnOffLightGrid() turns a range 
+   of lights in the lightGrid on or off */ 
 
 fun turnOnOffLightGrid(lightGrid: Array<Array<Boolean>>, x_start: Int, y_start: Int, x_end: Int, y_end: Int, turnOn: Boolean) {
     for(i in x_start .. x_end)
@@ -52,11 +63,17 @@ fun turnOnOffLightGrid(lightGrid: Array<Array<Boolean>>, x_start: Int, y_start: 
             lightGrid[i][j] = turnOn
 }
 
+/* toggleLightGrid() turns a range of lights in the 
+   lightGrid the opposite of the current state */
+
 fun toggleLightGrid(lightGrid: Array<Array<Boolean>> , x_start: Int, y_start: Int, x_end: Int, y_end: Int) {
     for(i in x_start .. x_end)
         for(j in y_start .. y_end)
             lightGrid[i][j] = !lightGrid[i][j]
 }
+
+/* countIlluminatedLights() returns the
+   total number of lights that are on */
 
 fun countIlluminatedLights(lightGrid: Array<Array<Boolean>>): Int {
     var count = 0
