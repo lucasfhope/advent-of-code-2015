e357256c138b7696c03c98646fcc0f0ee1bd4470
Lucas Hope <lucasfhope@icloud.com>
2023-08-22 20:34:56 -0400

    code update

--- day1/day1.kt
+++ day1/day1.kt
@@ -3,7 +3,6 @@ import java.io.File
 import java.io.InputStream
 
 fun main() {
-
     val inputStream: InputStream = File("input.txt").inputStream()
     val instructions = inputStream.bufferedReader().use { it.readText() }
 
@@ -11,24 +10,14 @@ fun main() {
     var instruction_number = 0
     var basement_enetered = false
 
-    /* changes the floor number based on the instruction 
-       and checks for the first time the basement is entered */
-
     for(instruction in instructions) {
-        
         instruction_number++
-
-        if(instruction == '(') {
-            floor++
-        } else if (instruction == ')') {
-            floor--
-        }
-
+        if(instruction == '(') floor++
+		else if (instruction == ')') floor--
         if(floor == -1 && !basement_enetered) {
-            println("Basement first enetered after instruction $instruction_number") 
+            println("Basement entered after instruction $instruction_number")
             basement_enetered = true
         } 
     }
-
-    println("Santa finished on floor $floor")
+    println("Instructions lead to floor $floor")
 }
